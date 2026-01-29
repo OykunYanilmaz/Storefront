344e1318161b2bc8475c4f5148480326133acfcc
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-01-29 21:45:25 +0300

    Switch database to MySql and update dependencies

--- storefront/settings.py
+++ storefront/settings.py
@@ -85,9 +85,16 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Mysql_84'
     }
 }
 

114b26e7dd2edf8c7938bd9dd8c88eaf35fe9440
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-01-30 11:38:14 +0300

    Django ORM codes

--- playground/views.py
+++ playground/views.py
@@ -1,19 +1,141 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q, F, Value, Func, ExpressionWrapper, DecimalField
+from django.db.models.functions import Concat
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+from django.db import transaction, connection
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product, OrderItem, Order, Customer, Collection
+from tags.models import TaggedItem
 
 # Create your views here.
-# request -> response
-# request handler
-# action
-
-def calculate():
-    x = 1
-    y = 2
-    return x
 
 
+# @transaction.atomic()
 def say_hello(request):
+    # query_set[0:5]
+    # list(query_set)
+    # for product in query_set:
+    #     print(product)
+
+    # queryset = Product.objects.filter(unit_price__range=(20, 30))
+    # queryset = Product.objects.filter(collection__id__range=(1, 2, 3))
+    # queryset = Product.objects.filter(title__icontains="coffee")
+    # queryset = Product.objects.filter(last_update__year=2021)
+    # queryset = Product.objects.filter(description__isnull=True)
+
+    # queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20) # and operator
+    # queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20) # and operator
+    # queryset = Product.objects.filter(
+
+    # Q Object
+    # queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))  # or operator
+    # queryset = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20))  # not operator
+
+    # F Object
+    # queryset = Product.objects.filter(inventory=F('unit_price'))
+
+    # - desc order
+    # queryset = Product.objects.order_by("unit_price", "-title").reverse()
+
+    # Sorting
+    # product = Product.objects.order_by("unit_price")[0] # pick the first
+    # product = Product.objects.earliest("unit_price")
+
+    # Limiting Results
+    # queryset = Product.objects.all()[:5]  # 0,1,2,3,4
+    # queryset = Product.objects.all()[5:10]  # 5,6,7,8,9
+
+    # Selecting Fields
+    # queryset = Product.objects.values("id", "title", "collection__title")  # inner join
+    # queryset = Product.objects.values_list("id", "title", "collection__title")
+    # queryset = Product.objects.filter(id__in=OrderItem.objects.values("product_id").distinct()).order_by("title")
+
+    # Deferring Fields
+    # queryset = Product.objects.only("id", "title")  # careful many queries
+    # queryset = Product.objects.defer("description")
+
+    # Selecting Related Objects
+    # select_related (1)
+    # queryset = Product.objects.select_related("collection__someOtherField").all()  # inner join
+    # prefetch_related (n)
+    # queryset = Product.objects.prefetch_related("promotions").select_related("collection").all()
+    # queryset = Order.objects.select_related("customer").prefetch_related("orderitem_set__product").order_by("-placed_at")[:5]
+
+    # Aggregate Objects
+    # result = Product.objects.filter(collection__id=2).aggregate(count=Count("id"), min_price=Min("unit_price"))
+
+    # Annotating Objects
+    # queryset = Customer.objects.annotate(is_new=Value(True))
+    # queryset = Customer.objects.annotate(new_id=F("id") + 1)
+    # queryset = Customer.objects.annotate(full_name=Func(F("first_name"), Value(" "), F("last_name"), function="CONCAT"))
+    # queryset = Customer.objects.annotate(full_name=Concat("first_name", Value(" "), "last_name"))
+
+    # Grouping Data
+    # queryset = Customer.objects.annotate(orders_count=Count("order"))
+
+    # Expression Wrapper
+    # queryset = Product.objects.annotate(discounted_price=F("unit_price") * 0.8)
+    # discounted_price = ExpressionWrapper(F("unit_price") * 0.8, output_field=DecimalField())
+    # queryset = Product.objects.annotate(discounted_price=discounted_price)
+
+    # Querying Generic Relationships
+    # content_type = ContentType.objects.get_for_model(Product)  # represents the row of product model in django_content_type table
+    # queryset = TaggedItem.objects.select_related("tag") \
+    #                              .filter(content_type=content_type, object_id=1)
+
+    # Custom Managers
+    # TaggedItem.objects.get_tags_for(Product, 1)
+
+    # QuerySet Cache
+    # queryset = Product.objects.all()
+    # queryset[0]
+    # list(queryset)
+
+    # Creating Objects
+    # collection = Collection()
+    # collection.title = "Video Games"
+    # collection.featured_product = Product(pk=1)  # same as collection.featured_product_id = 1
+    # collection.save()
+    # collection = Collection.objects.create(name="a", featured_product_id=1)  # same as the four lines above
+
+    # Updating Objects
+    # collection = Collection(pk=11)
+    # collection.title = "Games"
+    # collection.featured_product = None
+    # collection.save()
+    # collection = Collection.objects.get(pk=11)
+    # collection.featured_product = Product(pk=1)
+    # collection.save()
+    # Collection.objects.filter(pk=11).update(featured_product=None)
+
+    # Deleting Objects
+    # collection = Collection(pk=11)
+    # collection.delete()
+    # collection.objects.filter(id__gt=5).delete()
+
+    # Transactions
+    # with transaction.atomic():
+    #     order = Order()
+    #     order.customer_id = 1
+    #     order.save()
+
+    #     item = OrderItem()
+    #     item.order = order
+    #     item.product_id = -1
+    #     item.quantity = 1
+    #     item.unit_price = 10
+    #     item.save()
+
+    # Executing Raw SQL Queries
+    queryset = Product.objects.raw("SELECT id, title FROM store_product")
+    # with connection.cursor() as cursor:
+    #     cursor.execute("")
+    #     cursor.callproc("get_customers", [1, 2, 'a'])  # sp
+
     # return HttpResponse("Hello World")
-    x = calculate()
-    return render(request, "hello.html", {"name": "Oykun"})
+    # return render(request, "hello.html", {"name": "Oykun", "orders": list(queryset)})
+    return render(request, "hello.html", {"name": "Oykun", "result": list(queryset)})
+    # return render(request, "hello.html", {"name": "Oykun", "tags": list(queryset)})
+    # return render(request, "hello.html", {"name": "Oykun"})
--- store/models.py
+++ store/models.py
@@ -10,8 +10,7 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
-    featured_product = models.ForeignKey(
-        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Product(models.Model):
@@ -42,8 +41,7 @@ class Customer(models.Model):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
-    membership = models.CharField(
-        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
     # class Meta:
     #     # db_table = 'store_customers'
@@ -64,8 +62,7 @@ class Order(models.Model):
     ]
 
     placed_at = models.DateTimeField(auto_now_add=True)
-    payment_status = models.CharField(
-        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 

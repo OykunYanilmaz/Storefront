35698dc2d40d7b01e631194b174b94de82d85f6c
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-01-30 17:46:26 +0300

    Admin site chapter

--- storefront/urls.py
+++ storefront/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
 
-# playground/hello
-# path -> chop off the first part hello remains
+
+admin.site.site_header = 'Storefront Admin'
+admin.site.index_title = 'Admin'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),

a027bce879079aad4d6aaf0f0a19cf95cd563a55
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-02 18:34:50 +0300

    Initial commit for the third part

--- storefront/settings.py
+++ storefront/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront',
+        'NAME': 'storefront2',
         'HOST': 'localhost',
         'USER': 'root',
         'PASSWORD': 'Mysql_84'

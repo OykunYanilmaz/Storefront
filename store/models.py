101bad4680f5f895266d2a814fb7328b04493660
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-03 10:51:11 +0300

    Validating uploaded files

--- store/models.py
+++ store/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, FileExtensionValidator
 from uuid import uuid4
 from django.conf import settings
 from django.contrib import admin
+from store.validators import validate_file_size
 
 # Create your models here.
 
@@ -45,7 +46,8 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
-    image = models.ImageField(upload_to='store/images')  # media/store/images
+    image = models.ImageField(upload_to='store/images', validators=[validate_file_size])  # media/store/images
+    # image = models.FileField(upload_to='store/images', validators=[FileExtensionValidator(allowed_extensions=['pdf'])])
 
 
 class Customer(models.Model):

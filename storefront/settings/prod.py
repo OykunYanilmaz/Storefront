12cfb57a4ffc1c6027617cff68faab20e6eeeb19
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-05 12:46:14 +0300

    Final commit for this django course

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -3,6 +3,6 @@ from .common import *
 
 DEBUG = False
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']  # https://djecrety.ir/ # heroku config:set SECRET_KEY='key value'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []  # heroku place

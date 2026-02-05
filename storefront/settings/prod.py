08aff84abde39529156104c14e3de02c1fecd223
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-05 20:31:34 +0300

    Configure Postgres via dj-database-url for Render

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from .common import *
 
 DEBUG = False
@@ -8,3 +9,12 @@ DEBUG = False
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
+
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=False,
+    )
+}
